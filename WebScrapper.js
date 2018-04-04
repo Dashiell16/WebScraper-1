1900ffc9eb8d2f16c7261f1a27df1b85e8c4e7ed
Daniel Smith <15075097@stu.mmu.ac.uk>
2018-04-04 16:46:32 +0100

    Update WebScrapper.js

--- WebScrapper.js
+++ WebScrapper.js
@@ -2,23 +2,31 @@ var knwl = require('./node_modules/knwl.js/knwl.js'),
     knwlInstance = new knwl('english'),
     request = require('request');
 
-knwlInstance.init("07795066161 is my phone number and daniel.0397@live.com is my email");
-var phones = knwlInstance.get('phones'),
-    emails = knwlInstance.get('emails');
-
-console.log(phones);
-console.log(emails);
-
-
 request("https://www.canddi.com", function(err, response, body) {
     if (err) {
         console.log(err);
     }
     else {
         knwlInstance.init(body);
-        var phones2 = knwlInstance.get('phones'),
-        emails2 = knwlInstance.get('emails');
-        console.log(phones2);
-        console.log(emails2);
+        var arrPhones = knwlInstance.get('phones') ,
+        arrEmails = knwlInstance.get('emails') ;
+        if(arrPhones.length == 0){
+            console.log("No phone numbers found");
+        }
+        else{
+            for(var i = 0; i <= (arrPhone.length - 1); i++){
+                delete arrPhone[i]['preview'];
+            }
+            console.log(arrPhones);
+        }
+        if(arrEmails.length == 0){
+            console.log("No email addresses found");
+        }
+        else{
+            for(var i = 0; i <= (arrEmails.length - 1); i++){
+                delete arrEmails[i]['preview'];
+            }
+            console.log(arrEmails);
+        }
     }
 });
