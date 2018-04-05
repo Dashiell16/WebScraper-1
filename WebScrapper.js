c43cb8b8bd076d6b4a54b5da10a972a5618a0094
Daniel Smith <15075097@stu.mmu.ac.uk>
2018-04-05 13:32:17 +0100

    Update WebScrapper.js

--- WebScrapper.js
+++ WebScrapper.js
@@ -1,32 +1,49 @@
 var knwl = require('./node_modules/knwl.js/knwl.js'),
     knwlInstance = new knwl('english'),
+    readline = require('readline'),
     request = require('request');
 
-request("https://www.canddi.com", function(err, response, body) {
-    if (err) {
-        console.log(err);
-    }
-    else {
-        knwlInstance.init(body);
-        var arrPhones = knwlInstance.get('phones') ,
-        arrEmails = knwlInstance.get('emails') ;
-        if(arrPhones.length == 0){
-            console.log("No phone numbers found");
+var rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Input an email address: ', function(strInput) {
+    console.log(" ");
+    var arrInput = strInput.split("@");
+    
+    request("https://www." + arrInput[1], function(err, response, body) {
+        if (err) {
+            console.log(err);
         }
-        else{
-            for(var i = 0; i <= (arrPhone.length - 1); i++){
-                delete arrPhone[i]['preview'];
+        else {
+            knwlInstance.init(body);
+            var arrPhones = knwlInstance.get('phones') ,
+            arrEmails = knwlInstance.get('emails') ;
+        
+            if(arrPhones.length == 0){
+                console.log("No phone numbers found");
+                console.log("------------------------------");
             }
-            console.log(arrPhones);
-        }
-        if(arrEmails.length == 0){
-            console.log("No email addresses found");
-        }
-        else{
-            for(var i = 0; i <= (arrEmails.length - 1); i++){
-                delete arrEmails[i]['preview'];
+            else{
+                for(var i = 0; i <= (arrPhone.length - 1); i++){
+                    delete arrPhone[i]['preview'];
+                    delete arrPhone[i]['found'];
+                }
+                console.log(arrPhones);
+                console.log("------------------------------");
             }
-            console.log(arrEmails);
+            if(arrEmails.length == 0){
+                console.log("No email addresses found");
+            }
+            else{
+                for(var i = 0; i <= (arrEmails.length - 1); i++){
+                    delete arrEmails[i]['preview'];
+                    delete arrEmails[i]['found'];
+                }
+                console.log(arrEmails);
+            }
         }
-    }
+    });
+    rl.close();
 });
